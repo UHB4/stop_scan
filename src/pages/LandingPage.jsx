cfea3dbc67e5dfeb43a9d37668db40a76cc68824
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-23 14:17:03 +0900

    랜딩페이지 keyHole 이미지 애니메이션 1차구현
    위아래로 움직이는거 무한반복

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React,{ useEffect, useRef} from 'react';
+import gsap from 'gsap';
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 import styles from './_LandingPage.module.scss';
 import keyHole from '../assets/landing/carKeyHole.jpg'
 
 export default function LandingPage() {
+    gsap.registerPlugin(ScrollTrigger);
+    const imgRef = useRef(null);
+
+    useEffect(() => {
+        gsap.to(imgRef.current, {
+            opacity:1,
+            y: -20, // y축으로 20이동
+            repeat: -1, // 무한반복
+            yoyo: true, // 다시 원래위치로 돌아옴
+            duration: 1,
+            ease: 'power1.inOut',
+
+        })
+    }, []);
+
+
     return (
         <div className={styles.landingPage}>
-            <div className={styles.holeBox}>
-                <img src={keyHole} alt="keyHole" />
+            <div className={styles.section1}>
+                <div className={styles.holeBox}>
+                <img ref={imgRef}src={keyHole} alt="keyHole" />
+                </div>
             </div>
         </div>
     );
