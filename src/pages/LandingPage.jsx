9789326810586f8c9211de967d9ce85cea9b30f3
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-23 13:10:55 +0900

    keyHole 이미지추가 landingPage에 삽입
    투명도 낮췄음

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './_LandingPage.module.scss';
+import keyHole from '../assets/landing/carKeyHole.jpg'
 
 export default function LandingPage() {
     return (
         <div className={styles.landingPage}>
-            <div className={styles.card}>
-                <h1 className={styles.title}>Welcome to the Landing Page</h1>
-                <p className={styles.description}>This is a simple description for the landing page.</p>
+            <div className={styles.holeBox}>
+                <img src={keyHole} alt="keyHole" />
             </div>
         </div>
     );
