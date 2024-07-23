9feef3cd73c6ab0fca53a489a0cfbdbb524351a1
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-23 12:50:17 +0900

    기본 scss설정
    assets->fonts->폰트추가
    
    styles에 scss관련 설정 추가

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import styles from '../styles/_LandingPage.module.scss'
+import styles from './_LandingPage.module.scss';
 
 export default function LandingPage() {
     return (
-        <div className={styles.mainPage}>
+        <div className={styles.landingPage}>
+            <div className={styles.card}>
+                <h1 className={styles.title}>Welcome to the Landing Page</h1>
+                <p className={styles.description}>This is a simple description for the landing page.</p>
+            </div>
         </div>
-    )
+    );
 }
