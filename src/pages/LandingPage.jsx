2f2eda3b781c45d94a7f7d2a8732847ea4b5290c
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-23 11:12:18 +0900

    1차 폴더구조 세팅 랜딩페이지 리허설

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../styles/_LandingPage.module.scss'
+
+export default function LandingPage() {
+    return (
+        <div className={styles.mainPage}>
+        </div>
+    )
+}
