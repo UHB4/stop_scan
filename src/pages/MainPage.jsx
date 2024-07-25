2a6f5d8775fbe442690d4121506419563da5b27b
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-25 13:47:51 +0900

    로드될때 section이 밀려나며 공백이 생기는 문제 해결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,12 +5,47 @@ import styles from './_MainPage.module.scss';
 
 
 export default function MainPage() {
-    const section1Ref = useRef(null)
+    const section1Ref = useRef(null);
+    const wheelRef = useRef(null);
+
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        const wheel = wheelRef.current;
+        const section1 = section1Ref.current;
+
+        const wheelRotate = gsap.timeline();
+        wheelRotate.to(wheel, {
+            rotation: 120,
+            duration: 1,
+            ease: 'none',
+        })
+        ScrollTrigger.create({
+            animation: wheelRotate,
+            trigger: section1,
+            start: " top top",
+            end: "+=2000",
+            scrub: true,
+            pin: true,
+            // pinSpacing: false,
+            anticipatePin: 1,
+            markers: true,
+        });
+
+        return () => {
+
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+
+        };
+
+
+    }, []);
 
     return (
+
         <main className={styles.content}>
             <section ref={section1Ref} className={`${styles.stopscanItems} ${styles.section1}`}>
-                <div className={styles.wheel}></div>
+                <div ref={wheelRef} className={styles.wheel}></div>
             </section>
             <section className={`${styles.stopscanItems} ${styles.section2}`}>
 
