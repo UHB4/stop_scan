cff6a655954b2f01488e2d3eeeef3e876eaa9361
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-25 14:39:08 +0900

    car move 애니메이션 구현

---
+++ src/components/Section1.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, forwardRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import styles from '../pages/_MainPage.module.scss';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Section1 = forwardRef((props, ref) => {
+    const wheelRef = useRef(null);
+
+    useEffect(() => {
+        const wheel = wheelRef.current;
+        const section1 = ref.current;
+
+        const wheelRotate = gsap.timeline();
+        wheelRotate.to(wheel, {
+            rotation: 120,
+            duration: 1,
+            ease: 'none',
+        });
+
+        gsap.to(wheel, {
+            y: -20,
+            yoyo: true,
+            repeat: -1,
+            duration: 1,
+            ease: 'power2.inOut',
+        });
+
+        ScrollTrigger.create({
+            animation: wheelRotate,
+            trigger: section1,
+            start: "top top",
+            end: "+=800",
+            scrub: true,
+            pin: true,
+            anticipatePin: 1,
+            // markers: true,
+        });
+
+        return () => {
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        };
+    }, [ref]);
+
+    return (
+        <section ref={ref} className={`${styles.stopscanItems} ${styles.section1}`}>
+            <div ref={wheelRef} className={styles.wheel}></div>
+        </section>
+    );
+});
+
+export default Section1;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,78 +1,69 @@
-import React, { useEffect, useRef, useState } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useEffect, useRef } from 'react';
 import styles from './_MainPage.module.scss';
+import Section1 from '../components/Section1';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
 export default function MainPage() {
     const section1Ref = useRef(null);
-    const wheelRef = useRef(null);
-
+    const section2Ref = useRef(null);
+    const carRRef = useRef(null);
 
     useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        const wheel = wheelRef.current;
         const section1 = section1Ref.current;
+        const section2 = section2Ref.current;
+        const carRside = carRRef.current;
 
-        const wheelRotate = gsap.timeline();
-        wheelRotate.to(wheel, {
-            rotation: 120,
-            duration: 1,
-            ease: 'none',
-        })
-        ScrollTrigger.create({
-            animation: wheelRotate,
-            trigger: section1,
-            start: " top top",
-            end: "+=2000",
-            scrub: true,
-            pin: true,
-            // pinSpacing: false,
-            anticipatePin: 1,
-            markers: true,
-        });
+        if (section1 && section2 && carRside) {
+            // Section1의 애니메이션 끝나는 지점을 트리거로 사용
+            const section1EndTrigger = ScrollTrigger.create({
+                trigger: section1,
+                start: "top top",
+                end: "+=800",  // Section1의 end 값과 일치시킴
+                scrub: true,
+            });
+
+            // Section2의 carMove 애니메이션
+            const carMove1 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: section2,
+                    start: "top top",
+                    end: "+=1800",
+                    scrub: true,
+                    pin: true,
+                    anticipatePin: 1,
+                    markers: true,
+                    // Section1의 애니메이션이 끝난 후에 시작
+                    onEnter: () => section1EndTrigger.disable(),
+                }
+            });
+
+            carMove1.fromTo(carRside,
+                { x: '-300%', y: '50%' },
+                { x: '280%', duration: 1 }
+            );
+        }
 
         return () => {
-
             ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-
         };
-
-
     }, []);
 
     return (
-
         <main className={styles.content}>
-            <section ref={section1Ref} className={`${styles.stopscanItems} ${styles.section1}`}>
-                <div ref={wheelRef} className={styles.wheel}></div>
+            <Section1 ref={section1Ref} />
+            <section ref={section2Ref} className={`${styles.stopscanItems} ${styles.section2}`}>
+                <div ref={carRRef} className={styles.carRSide}></div>
             </section>
-            <section className={`${styles.stopscanItems} ${styles.section2}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section3}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section4}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section5}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section6}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section7}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section8}`}>
-
-            </section>
-            <section className={`${styles.stopscanItems} ${styles.section9}`}>
-
-            </section>
-
-
+            <section className={`${styles.stopscanItems} ${styles.section3}`}></section>
+            <section className={`${styles.stopscanItems} ${styles.section4}`}></section>
+            <section className={`${styles.stopscanItems} ${styles.section5}`}></section>
+            <section className={`${styles.stopscanItems} ${styles.section6}`}></section>
+            <section className={`${styles.stopscanItems} ${styles.section7}`}></section>
+            <section className={`${styles.stopscanItems} ${styles.section8}`}></section>
+            <section className={`${styles.stopscanItems} ${styles.section9}`}></section>
         </main>
-    )
+    );
 }
