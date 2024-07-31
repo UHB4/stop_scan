6be1ce923201459429f3da8aaf13d8ef234148bf
UHB4 <153577215+UHB4@users.noreply.github.com>
2024-07-31 10:41:16 +0900

    메뉴페이지 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './_Header.module.scss';
 
 const Header = ({ text }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState);
+    };
+
+    useEffect(() => {
+        console.log("토글 후 메뉴 상태: ", isMenuOpen);
+    }, [isMenuOpen]);
+
     return (
         <header className={styles.header}>
-            <h1>{text}</h1>
+            <div className={styles.inner}>
+                <div className={styles.leftPlaceholder}></div>
+                <h1 className>{text}</h1>
+                <div
+                    className={`${styles.rightBtn} ${isMenuOpen ? styles.on : ''}`}
+                    onClick={toggleMenu}
+                >
+                </div>
+            </div>
+            {isMenuOpen && (
+                <div className={styles.menuPage} onClick={toggleMenu}>
+                    <ul onClick={(e) => e.stopPropagation()}>
+                        <h1>STOP SCAN</h1>
+                        <li>휴게소 안내</li>
+                        <li>충전소 안내</li>
+                        <li>주유소 안내</li>
+                    </ul>
+                </div>
+            )}
         </header>
     );
 };
